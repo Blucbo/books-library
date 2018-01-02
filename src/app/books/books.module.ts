10aaca24dacb0315c4a01fac7e4fb7d3854a6f58
Bogdan <skochkobo@gmail.com>
2018-01-02 20:00:20 +0200

    Create template books and book.

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 import { BooksRoutingModule } from './books-routing.module';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BooksComponent } from './books.component';
 import { BookComponent } from './book/book.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatListModule,
+    MatIconModule,
+    MatTooltipModule,
     BooksRoutingModule
   ],
   declarations: [BooksComponent, BookComponent]
